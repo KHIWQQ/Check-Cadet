5d95f9fc7fb9ecd20ddee9d2a080f30552a30c5f
KHIWQQ <eiq1627@gmail.com>
2022-12-01 07:54:35 +0700

    01V2

---
+++ check_cadet.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2022 at 01:53 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `check_cadet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cadet`
+--
+
+CREATE TABLE `cadet` (
+  `id` int(5) NOT NULL,
+  `uname` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `lname` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `stay` int(2) NOT NULL,
+  `out` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cadet`
+--
+
+INSERT INTO `cadet` (`id`, `uname`, `lname`, `stay`, `out`) VALUES
+(1, 'A', 'B', 1, 0),
+(2, 'B', 'C', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cadet`
+--
+ALTER TABLE `cadet`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
