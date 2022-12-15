c0e96f9c91775f8ac3e823a6fd982f70c71c6250
KHIWQQ <eiq1627@gmail.com>
2022-12-15 23:32:52 +0700

    sql

--- batt.sql
+++ batt.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 02, 2022 at 04:08 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `check_cadet`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `batt`
---
 
 CREATE TABLE `batt` (
   `lname` varchar(255) NOT NULL,
@@ -34,10 +9,6 @@ CREATE TABLE `batt` (
   `outc` int(11) DEFAULT NULL,
   `com` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `batt`
---
 
 INSERT INTO `batt` (`lname`, `uname`, `stay`, `outc`, `com`) VALUES
 ('กรมถิน', 'ธีรเดช', 1, 0, 2),
@@ -292,17 +263,8 @@ INSERT INTO `batt` (`lname`, `uname`, `stay`, `outc`, `com`) VALUES
 ('ไทรชมภู', 'ธราธร', 1, 0, 2),
 ('ไสยสมบัติ', 'พัทธ์', 1, 0, 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `batt`
---
 ALTER TABLE `batt`
   ADD PRIMARY KEY (`lname`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
